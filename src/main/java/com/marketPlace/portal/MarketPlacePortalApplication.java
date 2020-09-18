49c99f15c349ccdc44cbf1e93a05b9bc52007e3d
Akshay Babbar <akshaybabbar12@gmail.com>
2020-09-18 16:42:43 +0530

    Initial Project Structure and SpringBoot Compliant

---
+++ src/main/java/com/marketPlace/portal/MarketPlacePortalApplication.java
@@ -0,0 +1,12 @@
+package com.marketPlace.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlacePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPlacePortalApplication.class, args);
+    }
+}
---
+++ src/main/java/com/marketPlace/portal/entity/TbAddress.java
@@ -0,0 +1,55 @@
+package com.marketPlace.portal.entity;
+
+import java.io.Serializable;
+
+public class TbAddress implements Serializable {
+    private Long addressId;
+
+    private String tel;
+
+    private String streetName;
+
+    private long Zip;
+
+    Boolean isPermanent;
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public long getZip() {
+        return Zip;
+    }
+
+    public void setZip(long zip) {
+        Zip = zip;
+    }
+
+    public Boolean isPermanent() {
+        return isPermanent;
+    }
+
+    public void setPermanent(Boolean permanent) {
+        isPermanent = permanent;
+    }
+}
