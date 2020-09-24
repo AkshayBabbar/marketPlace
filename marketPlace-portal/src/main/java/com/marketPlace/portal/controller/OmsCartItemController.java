e31624ec3e4e05f40a831eefa18f92ca0e1d8ea1
Akshay Babbar <akshaybabbar12@gmail.com>
2020-09-24 22:18:24 +0530

    security config to be added.

---
+++ marketPlace-portal/src/main/java/com/marketPlace/portal/controller/OmsCartItemController.java
@@ -0,0 +1,13 @@
+package com.marketPlace.portal.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Api(tags = "OmsCartItemController")
+
+@RequestMapping("/cart")
+public class OmsCartItemController {
+}
