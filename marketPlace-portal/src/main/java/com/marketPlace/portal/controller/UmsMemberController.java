3cac06e8108dd73099cdfb6d7fb11d1b17127c07
Akshay Babbar <akshaybabbar12@gmail.com>
2020-09-30 23:11:31 +0530

    UMS Controller added

--- marketPlace-portal/src/main/java/com/marketPlace/portal/controller/UmsMemberController.java
+++ marketPlace-portal/src/main/java/com/marketPlace/portal/controller/UmsMemberController.java
@@ -1,6 +1,21 @@
 package com.marketPlace.portal.controller;
 
+import com.marketPlace.portal.service.UmsMemberService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Api(tags = "UMSMemberController")
+@RequestMapping("/sso")
 public class UmsMemberController {
 
+    private String tokenHeader;
+
+    private String tokenHead;
+
+    @Autowired
+    private UmsMemberService memberService;
 
 }
