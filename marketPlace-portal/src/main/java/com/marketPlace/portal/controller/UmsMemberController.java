aba23e4e8877654423f2533ee2ade8062afcbfc8
Akshay Babbar <akshaybabbar12@gmail.com>
2020-10-02 22:40:43 +0530

    UMS Controller added

--- marketPlace-portal/src/main/java/com/marketPlace/portal/controller/UmsMemberController.java
+++ marketPlace-portal/src/main/java/com/marketPlace/portal/controller/UmsMemberController.java
@@ -2,20 +2,37 @@ package com.marketPlace.portal.controller;
 
 import com.marketPlace.portal.service.UmsMemberService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import marketPlace.commons.api.CommonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Api(tags = "UMSMemberController")
 @RequestMapping("/sso")
 public class UmsMemberController {
 
+    @Value("${jwt.tokenHeader}")
     private String tokenHeader;
-
+    @Value("${jwt.tokenHead}")
     private String tokenHead;
 
     @Autowired
     private UmsMemberService memberService;
 
+    @ApiParam(value = "Sign Up")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult register(@RequestParam String username,
+                                 @RequestParam String password,
+                                 @RequestParam String telephone,
+                                 @RequestParam String authCode) {
+        memberService.register(username, password, telephone, authCode);
+        return CommonResult.success(null, "registration success");
+    }
 }
