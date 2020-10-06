cddc32e725a2958f53ce8005a81bd4a1797496c3
Akshay Babbar <akshaybabbar12@gmail.com>
2020-10-06 22:17:09 +0530

    OrderParam added to this Repo.

---
+++ marketPlace-commons/src/main/java/com/marketPlace/commons/config/BaseRedisConfig.java
@@ -0,0 +1,9 @@
+package com.marketPlace.commons.config;
+
+//import org.springframework.data.redis.core.RedisTemplate;
+
+public class BaseRedisConfig {
+
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate
+}
